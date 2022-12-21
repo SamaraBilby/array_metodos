42bc331258555f63301e87c31a83bd51f78302f4
SamaraBilby <samarabilby@hotmail.com>
2022-12-21 16:14:28 -0300

    consumindo API ttps://randomuser.me/api com async await fetch

---
+++ script/script.js
@@ -0,0 +1,34 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const shortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+//random user and money
+
+async function getRandomUser() {
+
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+   // console.log(data) o que iremos usar é o results;
+
+   const user = data.results[0];
+    //console.log(user);
+
+   const newUser = {
+     name: `${user.name.first} ${user.name.last}`,
+     money: Math.floor(Math.random() * 1000000)
+   };
+   console.log(newUser);
+}
+
+// add um novo obj nos dados de array
+
